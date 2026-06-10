7fe265222a6cd7d95707c38307e9796f7778a0c5
mleszczynski-dev <mleszczynski.dev@gmail.com>
2026-06-10 22:06:58 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-echo "Building..."
+echo "Building... pwd: $(pwd)"
+
+which g++
+which cmake
+
+src_dir="cpp"
+build_type="release"
+
+rm -rf ./build-$build_type
+rm -rf ./install-$build_type
+
+cmake -S ./$src_dir/ -B ./build-$build_type -G Ninja -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_INSTALL_PREFIX=./install-$build_type
+cmake --build ./build-$build_type/ -j 6
+cmake --install ./build-$build_type/
 
