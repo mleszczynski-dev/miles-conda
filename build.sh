660328842157aef96660bc2e2d56ccd660eb643a
mleszczynski-dev <mleszczynski.dev@gmail.com>
2026-06-11 02:36:41 +0200

    Fix style and minor issues

--- build.sh
+++ build.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-echo "Building... pwd: $(pwd)"
-
-which g++
-which cmake
-
-src_dir="cpp"
 build_type="release"
 
 rm -rf ./build-$build_type
 rm -rf ./install-$build_type
 
-cmake -S ./$src_dir/ -B ./build-$build_type -G Ninja -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_INSTALL_PREFIX=./install-$build_type
-cmake --build ./build-$build_type/ -j 6
+which cmake
+cmake -S . \
+      -B ./build-$build_type \
+      -DCMAKE_BUILD_TYPE=$build_type \
+      -DCMAKE_INSTALL_PREFIX=${PREFIX}
+cmake --build ./build-$build_type/ -j ${CPU_COUNT}
 cmake --install ./build-$build_type/
-
